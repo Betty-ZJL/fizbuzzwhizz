16e68541a08710845cbee40dd13c2f6ba1fe5396
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-29 17:30:12 +0800

    Update main.js

--- src/main/main.js
+++ src/main/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function fizbuzzwhizz(a, b, c){
-  // write your code here...
+    if((n.toString().indexOf(a.toString()))>-1){
+        return 'Fizz';
+    }else if(n%a==0&&n%b==0&&n%c==0){
+        return 'FizzBuzzWhizz'
+    }else if(n%a==0&&n%b==0){
+        return 'FizzBuzz';
+    }else if(n%a==0&&n%c==0){
+        return 'FizzWhizz'
+    }else if(n%b==0&&n%c==0){
+        return 'BuzzWhizz'
+    }else if (n%a==0){
+        return 'Fizz';
+    }else if(n%b==0){
+        return 'Buzz';
+    }else if(n%c==0){
+        return 'Whizz';
+    }else
+        return n;
 }
